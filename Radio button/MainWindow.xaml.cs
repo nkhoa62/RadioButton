c8aea5dc78a58c546eaecc52177cf697b5ffbe65
nkhoa62 <nkhoa62@gmail.com>
2019-10-02 13:50:23 +0700

    Add project files.

---
+++ Radio button/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Radio_button
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                if ((bool)addition.IsChecked)
+                {
+
+                    addValues();
+
+                }
+
+            }
+
+            catch (Exception caught)
+            {
+
+                expression.Text = "";
+
+                result.Text = caught.Message;
+
+            }
+        }
+        private void addValues()
+        {
+
+            int lhs = int.Parse(lhsOperand.Text);
+
+            int rhs = int.Parse(rhsOperand.Text);
+
+            int outcome = 0;
+
+            // TODO: cộng lhs và rhs, lưu trong outcome
+
+            outcome = lhs + rhs;
+
+            expression.Text = lhsOperand.Text + " + " + rhsOperand.Text;
+
+            result.Text = outcome.ToString();
+
+        }
+    }
+}
